ea57ba0c30cd7daa27bd4a23c15d120df6daaa37
Kræn Hansen <post@creen.dk>
2012-03-07 12:58:12 +0100

    Breaking everything

---
+++ Frontend/app/controllers/APIClient.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import api.requests.VoteRequest;
+import api.responses.VoteResponse;
+
+import play.mvc.*;
+import play.mvc.Http.StatusCode;
+
+public class APIClient extends Controller {
+    
+	public DefaultHttpClient client;
+	/**
+	 * Constructing a new API client, try to reuse these.
+	 */
+	public APIClient() {
+		client = new DefaultHttpClient();
+	}
+
+	public VoteResponse send(VoteRequest request) {
+		return (VoteResponse) this.send(new JSONObject(request));
+	}
+	
+	public JSONObject send(JSONObject request) {
+		return 
+	}
+}
