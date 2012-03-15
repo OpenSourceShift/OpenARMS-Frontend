859f66d9e48805b29548aec6fe55ebcfa070cc6d
Cronows <linel@mail.lv>
2012-03-15 15:04:56 +0200

    All variables and indexes with name "pollID" were changed to "token".

--- Frontend/app/models/QuestionResult.java
+++ Frontend/app/models/QuestionResult.java
@@ -3,7 +3,7 @@ package models;
 import play.db.jpa.Model;
 
 public class QuestionResult extends Model {
-	protected int pollId = 12;
+	protected int token = 12;
 	protected int questionId = 1214;
 	protected int votes = 103;
 	protected String question = "When cheese gets its picture taken, what does it say?";
