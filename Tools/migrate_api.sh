fbd30d8c7a2727c94dc7d2b652c40139ccec3a7d
Attila Sukosd <attila.sukosd@gmail.com>
2012-03-16 11:15:37 +0100

    Pushed the new service API .jar

--- Tools/migrate_api.sh
+++ Tools/migrate_api.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Started the migration of the API classes ..."
 DIR=`dirname $0`
-CLASSDIR=`readlink -f $DIR/../Service/tmp/classes/api/`
+CLASSDIR=`readlink -f $DIR/../Service/tmp/classes/`
 JARTARGET=`readlink -f $DIR/../Frontend/lib/openarms-api.jar`
 echo "Tool dir is located at $DIR"
 echo "Class dir is at $CLASSDIR"
@@ -9,6 +9,6 @@ echo "Target .jar file at $JARTARGET"
 
 cd $CLASSDIR
 echo $JARTARGET
-zip -r $JARTARGET .
+zip -r $JARTARGET api/
 
 
