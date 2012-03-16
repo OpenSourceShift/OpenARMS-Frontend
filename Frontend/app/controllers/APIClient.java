527472a41f3ab99eb8ae250b5dde19f9198c7343
Kræn Hansen <post@creen.dk>
2012-03-16 12:11:26 +0100

    Chanced the body of the send-method in the APIClient

--- Frontend/app/controllers/APIClient.java
+++ Frontend/app/controllers/APIClient.java
@@ -1,23 +1,40 @@
 package controllers;
 
+import java.io.IOException;
+
 import javax.naming.OperationNotSupportedException;
 
+import models.helpers.GsonHelper;
+
+import oauth.signpost.http.HttpRequest;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
-/*
-import api.requests.VoteRequest;
-import api.responses.VoteResponse;
-*/
+import api.Request;
 
+import play.Play;
 import play.mvc.*;
 import play.mvc.Http.StatusCode;
 
 public class APIClient extends Controller {
 	
 	public static APIClient singleton;
+	public static HttpHost host;
+	static {
+		host = new HttpHost(Play.configuration.getProperty("openarms.service_host"), Integer.parseInt(Play.configuration.getProperty("openarms.service_port")));
+	}
     
 	public DefaultHttpClient client;
 	/**
@@ -34,9 +51,45 @@ public class APIClient extends Controller {
 		return singleton;
 	}
 	
-	public api.Response send(api.Request request) {
-		// TODO: Serialize the request object and send it for the service.
-		// Also remember to do proper errorhandling
-		return null;
+	private HttpRequestBase getBaseRequest(api.Request request) throws Exception {
+		HttpRequestBase httpRequest;
+		if(request.method == Request.Method.GET) {
+			httpRequest = new HttpGet();
+		} else if(request.method == Request.Method.POST) {
+			httpRequest = new HttpPost();
+		} else if(request.method == Request.Method.PUT) {
+			httpRequest = new HttpPut();
+		} else if(request.method == Request.Method.DELETE) {
+			httpRequest = new HttpDelete();
+		} else {
+			throw new Exception("Unknown HTTP-method of the API-request.");
+		}
+		return httpRequest;
+	}
+	
+	private api.Response sendRequest(api.Request request) throws Exception {
+		String json = GsonHelper.toJson(request);
+		HttpRequestBase httpRequest = getBaseRequest(request);
+		ByteArrayEntity bae = new ByteArrayEntity(json.getBytes());
+		
+		// Adding the payload
+		if(httpRequest instanceof HttpPost) {
+			((HttpPost) httpRequest).setEntity(bae);
+		} else if(httpRequest instanceof HttpPut) {
+			((HttpPut) httpRequest).setEntity(bae);
+		}
+		
+		HttpResponse httpResponse = client.execute(host, httpRequest);
+		HttpEntity httpResponseEntity = httpResponse.getEntity();
+		// Check the response content-type.
+		if(httpResponseEntity.getContentType().getValue().equals("application/json")) {
+			return GsonHelper.fromJson(httpResponseEntity.getContent(), request.EXPECTED_RESPONSE);
+		} else {
+			throw new Exception("Http response didn't have the application/json content-type.");
+		}
+	}
+	
+	public static api.Response send(api.Request request) throws Exception {
+		return getInstance().sendRequest(request);
 	}
 }
