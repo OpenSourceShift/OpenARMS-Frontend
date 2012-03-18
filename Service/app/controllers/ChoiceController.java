ed8e502befb1bdc2561ba3be7fb93ab7fafc3ced
Ignacio Rodriguez <kronics777@gmail.com>
2012-03-18 12:35:45 +0100

    Choice Controller implemented.

---
+++ Service/app/controllers/ChoiceController.java
@@ -0,0 +1,92 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import models.Choice;
+import models.Poll;
+import api.Response.CreateChoiceResponse;
+import api.Response.CreatePollResponse;
+import api.entities.ChoiceJSON;
+import api.entities.PollJSON;
+import api.helpers.GsonHelper;
+import play.mvc.Controller;
+
+public class ChoiceController extends Controller{
+
+	public static void create() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(request.body));
+		
+        try {
+        	
+            String json = reader.readLine();
+            ChoiceJSON choicejson = GsonHelper.fromJson(json, ChoiceJSON.class);
+            Choice choice = choicejson.toChoice();
+            choice.save();
+            CreateChoiceResponse r = new CreateChoiceResponse(choice);
+        	String jsonresponse = GsonHelper.toJson(r);
+        	renderJSON(jsonresponse);
+	
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            renderJSON(new String());
+        }
+        
+    }
+	
+	public static void retrieve() {
+		String choiceid = params.get("id");
+		
+		Choice choice = Choice.find("byChoiceID", choiceid).first();
+		
+		if (choice == null) {
+		    renderJSON("The Choice does not exist!");
+		}
+		
+		CreateChoiceResponse r = new CreateChoiceResponse(choice);
+		String jsonresponse = GsonHelper.toJson(r);
+		
+		renderJSON(jsonresponse);
+	}
+	
+	public static void edit () {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(request.body));
+		String choiceid = params.get("id");
+		Choice originalchoice = Choice.find("byChoiceID", choiceid).first();
+		
+		try {
+        	
+            String json = reader.readLine();
+            ChoiceJSON choicejson = GsonHelper.fromJson(json, ChoiceJSON.class);
+            Choice editedchoice = choicejson.toChoice();
+
+            originalchoice.text = editedchoice.text;
+            
+            originalchoice.save();
+
+            CreateChoiceResponse r = new CreateChoiceResponse(originalchoice);
+        	String jsonresponse = GsonHelper.toJson(r);
+        	renderJSON(jsonresponse);
+            
+		} catch (IOException ex) {
+            ex.printStackTrace();
+            renderJSON(new String());
+        }
+	}
+	
+	public static void delete () {
+		String choiceid = params.get("id");
+		Choice choice = Choice.find("byChoiceID", choiceid).first();
+		choice.delete();
+		
+		choice.id = null;
+		choice.poll = null;
+		choice.text = null;
+		
+		CreateChoiceResponse r = new CreateChoiceResponse(choice);
+    	String jsonresponse = GsonHelper.toJson(r);
+    	renderJSON(jsonresponse);
+	}
+
+}
