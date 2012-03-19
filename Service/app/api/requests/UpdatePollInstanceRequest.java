7c84449cf63b4f3fdec669a7f4a39c8c56a3735f
Bernhard Oberndorfer <oberndorfer.bernhard@gmail.com>
2012-03-19 14:02:55 +0100

    update-requests update

--- Service/app/api/requests/UpdatePollInstanceRequest.java
+++ Service/app/api/requests/UpdatePollInstanceRequest.java
@@ -1,8 +1,18 @@
 package api.requests;
 
-public class UpdatePollInstanceRequest {
-/*
- * PUT /pollinstance/12345
+import api.entities.PollInstanceJSON;
 
- */
+public class UpdatePollInstanceRequest extends Request {
+	public static final Class EXPECTED_RESPONSE = UpdatePollInstanceResponse.class;
+	public PollInstanceJSON pollInstance;
+	public UpdatePollInstanceRequest (PollInstanceJSON p) {
+		this.pollInstance = p;
+	}
+	
+	
+	@Override
+	public String getURL() {
+		return "/pollinstance/" + pollInstance.id;
+	}
+
 }
