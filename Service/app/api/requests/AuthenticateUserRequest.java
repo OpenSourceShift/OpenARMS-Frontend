835a3b33e0835e546e42ad31da9f851d3cc13277
Kræn Hansen <post@creen.dk>
2012-03-19 15:51:55 +0100

    Changes to the authentication requests.

--- Service/app/api/requests/AuthenticateUserRequest.java
+++ Service/app/api/requests/AuthenticateUserRequest.java
@@ -3,12 +3,14 @@ package api.requests;
 import api.entities.UserJSON;
 import api.responses.AuthenticateUserResponse;
 
-public class AuthenticateUserRequest extends Request {
+public abstract class AuthenticateUserRequest extends Request {
 	public static final Class EXPECTED_RESPONSE = AuthenticateUserResponse.class;
 	public UserJSON user;
-	public AuthenticateUserRequest (UserJSON u) {
+	public String backend;
+	public AuthenticateUserRequest (UserJSON u, String backend) {
 		this.method = Method.POST;
 		this.user = u;
+		this.backend = backend;
 	}
 		
 	@Override
