a58f3761d58d742e12de573d592eeb6fc0e9857e
Bernhard Oberndorfer <oberndorfer.bernhard@gmail.com>
2012-03-19 11:31:59 +0100

    created even more api.requests classes

---
+++ Service/app/api/requests/InstantiatePollRequest.java
@@ -0,0 +1,19 @@
+package api.requests;
+
+import models.PollInstance;
+import api.entities.PollInstanceJSON;
+import api.responses.CreatePollInstanceResponse;
+
+public class InstantiatePollRequest extends Request {
+	public static final Class EXPECTED_RESPONSE = CreatePollInstanceResponse.class;
+	public PollInstanceJSON pollInstance;
+	public InstantiatePollRequest(PollInstance p) {
+		this.pollInstance = p.toJson();
+	}
+	@Override
+	public String getURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
