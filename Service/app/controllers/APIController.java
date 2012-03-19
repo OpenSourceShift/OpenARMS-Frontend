a7f8dba256bc57881aa763c25b85d438febb1fff
Kræn Hansen <post@creen.dk>
2012-03-19 09:46:08 +0100

    Added the abstract APIController

---
+++ Service/app/controllers/APIController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public abstract class APIController extends Controller {
+	
+	public static void renderException(Exception e) {
+		// TODO: Implement
+	}
+}
