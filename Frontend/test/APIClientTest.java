5b227ab18a779063d5ef7d5361c4b5bc2fe1bbcd
Ignacio Rodriguez <kronics777@gmail.com>
2012-03-20 15:37:01 +0100

    Added some minor changes and tests.

--- Frontend/test/APIClientTest.java
+++ Frontend/test/APIClientTest.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 import play.mvc.Http.StatusCode;
@@ -17,7 +20,7 @@ public class APIClientTest extends UnitTest {
     		fail("did not get the HTTP-OK status-code from the service, got "+response.statusCode+": "+response.error_message);
 		}
 	}
-
+/*
     @Test
     public void testCreateChoice() throws Exception {
     	ChoiceJSON c = new ChoiceJSON();
@@ -37,27 +40,38 @@ public class APIClientTest extends UnitTest {
     	assertNotNull(response.pollinstance.id);
     	assertEquals(response.pollinstance.poll_id, p.poll_id);
     }
-    
+  
+	@Test
+    public void testCreateUser() throws Exception {
+		UserJSON u = new UserJSON();
+		u.name = "test";
+		u.email = "avas@dfsdf.com";
+		u.secret = null;
+		u.attributes = new HashMap<String, String>();
+		u.attributes.put("password", "1234");
+
+		CreateUserResponse response = (CreateUserResponse) APIClient.send(new CreateUserRequest(u));
+
+    	failIfNotSuccessful(response);
+    	assertNotNull(response.user.id);
+    	assertEquals(response.user.name, u.name);
+    }  */
+	
     @Test
     public void testCreatePoll() throws Exception {
     	PollJSON p = new PollJSON();
+    	ChoiceJSON c = new ChoiceJSON();
+    	c.text = "TestChoice";
     	p.question = "poll question";
+    	p.reference = "Test";
+    	p.choices = new LinkedList();
+    	p.choices.add(c);
     	CreatePollResponse response = (CreatePollResponse) APIClient.send(new CreatePollRequest(p));
     	failIfNotSuccessful(response);
     	assertNotNull(response.poll.id);
     	assertEquals(response.poll.question, p.question);
     }
-    
-    @Test
-    public void testCreateUser() throws Exception {
-    	UserJSON u = new UserJSON();
-    	u.name = "user name";
-    	CreateUserResponse response = (CreateUserResponse) APIClient.send(new CreateUserRequest(u));
-    	failIfNotSuccessful(response);
-    	assertNotNull(response.user.id);
-    	assertEquals(response.user.name, u.name);
-    }
-    
+    /*
     @Test
     public void testCreateVote() throws Exception {
     	VoteJSON v = new VoteJSON();
@@ -67,5 +81,5 @@ public class APIClientTest extends UnitTest {
     	failIfNotSuccessful(response);
     	assertNotNull(response.vote.id);
     	assertEquals(response.vote.choiceid, v.choiceid);
-    }
+    }*/
 }
