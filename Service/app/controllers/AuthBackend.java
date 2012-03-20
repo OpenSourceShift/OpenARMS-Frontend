305336fd0f861107c04d1a62c7b48210d58fee34
Attila Sukosd <attila.sukosd@gmail.com>
2012-03-20 10:33:23 +0100

    getCurrentUser() added to AuthBackend

--- Service/app/controllers/AuthBackend.java
+++ Service/app/controllers/AuthBackend.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import models.User;
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.Header;
 
 /**
  * Abstract controller which specifies authentication method.
@@ -9,4 +11,21 @@ import play.mvc.*;
  */
 public abstract class AuthBackend extends Controller {
 
+	
+	public static User getCurrentUser() {
+		User u = null;		
+		Header header = Http.Request.current().headers.get("authorization");
+	    if (header != null) {
+	    	String user = Http.Request.current().user;
+	    	String secret = Http.Request.current().password;
+	    	
+	    	u = User.findById(Long.parseLong(user));
+	    	if (u.secret.equals(secret)) {
+	    		return u;
+	    	} else {
+	    		return null;
+	    	}
+	    }
+		return null;
+	}
 }
