1638aaf129e823de1814e90de4ae0c890c7b196f
Kræn Hansen <post@creen.dk>
2012-03-21 21:33:03 +0100

    Added a lot relating to the JoinPoll

--- Frontend/app/controllers/JoinPoll.java
+++ Frontend/app/controllers/JoinPoll.java
@@ -1,13 +1,13 @@
 package controllers;
-import java.util.Date;
-
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Http.StatusCode;
+import api.entities.PollInstanceJSON;
 import api.entities.PollJSON;
 import api.entities.VoteJSON;
 import api.requests.CreateVoteRequest;
 import api.requests.ReadPollInstanceByTokenRequest;
+import api.requests.ReadPollInstanceRequest;
 import api.requests.ReadPollRequest;
 import api.responses.ReadPollInstanceResponse;
 import api.responses.ReadPollResponse;
@@ -27,13 +27,10 @@ public class JoinPoll extends Controller {
 			notFound("Error finding the poll.");
 		}
 		
-		Long poll_id = res.pollinstance.poll_id;
-		// Long instanceId = instanceResponse.pollinstance.id;
-		// Date startDateTime = instanceResponse.pollinstance.startDateTime;
-		Date end = res.pollinstance.end;
+		PollInstanceJSON pollInstance = res.pollinstance;
 
 		// TODO: Consider if this is even nessesary to get the poll, as choices are allready defined in the instance response (as VoteSummaryJSONs).
-		ReadPollResponse pollResponse = (ReadPollResponse) APIClient.send(new ReadPollRequest(poll_id));
+		ReadPollResponse pollResponse = (ReadPollResponse) APIClient.send(new ReadPollRequest(pollInstance.poll_id));
 		if(pollResponse.statusCode.equals(StatusCode.NOT_FOUND)) {
 			notFound("Poll not found.");
 		} else if (StatusCode.error(pollResponse.statusCode)) {
@@ -41,63 +38,40 @@ public class JoinPoll extends Controller {
 		}
 		
 		PollJSON poll = pollResponse.poll;
-		render(poll);
+		render(poll, pollInstance);
 	}
 
-	public static void submit(Long instanceId, Long[] votes) throws Exception {
-		if (votes != null) {
-			for (Long l : votes) {
-				VoteJSON vote = new VoteJSON();
-				vote.choiceid = l;
-				vote.pollInstanceid = instanceId;
-				APIClient.getInstance().send(new CreateVoteRequest(vote));
-			}
-		}
-		else {
-			throw new Exception("no answers submitted");
-		}
-		
-		/* TODO: validation
-		validation.required(stuff);
-		if (validation.hasErrors()) {
-			params.flash();
+	public static void submit(String token, Long pollinstance_id, Long poll_id, Long[] answers) throws Exception {
+		validation.required("answers", answers);
+		validation.required("pollinstance_id", pollinstance_id);
+		if(validation.hasErrors()) {
+			// Show the user ...
 			validation.keep();
 			index(token);
-			return;
-		} */
-		
-		success(instanceId);
+		} else {
+			for (Long l : answers) {
+				VoteJSON vote = new VoteJSON();
+				vote.choiceid = l;
+				vote.pollInstanceid = pollinstance_id;
+				APIClient.getInstance().send(new CreateVoteRequest(vote));
+			}
+			success(pollinstance_id, poll_id);
+		}
 	}
 
-	public static void success(Long instanceId) {
-		// TODO: what should happen? just show the statistics of the instance?
-		
-		/* String question = null;
-		JsonArray answersArray = null;
-		String duration = null;
-
-		try {
-			ReadPollInstanceResponse response = (ReadPollInstanceResponse) APIClient.getInstance().send(new ReadPollInstanceRequest(instanceId));
-			question = response.question;
-			answersArray = response.answersArray;
-			duration = response.duration;
-			
-		} catch (Exception e) {
-			try {
-				// Most like the poll has been inactivated, so we need the results instead
-				GetResultsResponse response = (GetResultsResponse) APIClient.getInstance().send(new GetResultsRequest(token, null));
-				question = response.question;
-				answersArray = response.answersArray;
-				duration = "0";
-			} catch (Exception e2) {
-			}
+	public static void success(Long pollinstance_id, Long poll_id) throws Exception {
+		/** get the Poll Instance Data */
+		ReadPollResponse res1 = (ReadPollResponse) APIClient.send(new ReadPollRequest(poll_id));
+		ReadPollInstanceResponse res2 = (ReadPollInstanceResponse) APIClient.send(new ReadPollInstanceRequest(pollinstance_id));
+		if(res1.statusCode.equals(StatusCode.NOT_FOUND) || res2.statusCode.equals(StatusCode.NOT_FOUND)) {
+			notFound("Poll not found.");
+		} else if (StatusCode.error(res1.statusCode) || StatusCode.error(res2.statusCode)) {
+			notFound("Error finding the poll.");
 		}
 
-		render(token, question, answersArray, duration, durationString);
+		PollJSON poll = res1.poll;
+		PollInstanceJSON pollInstance = res2.pollinstance;
 		
-	}
-
-	public static void nopoll(String token) {
-		render(token); */
+		render(poll, pollInstance);
 	}
 }
