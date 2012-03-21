6314bbe7b8d75836812898daadcbba09fe9959e2
Attila Sukosd <attila.sukosd@gmail.com>
2012-03-21 20:43:06 +0100

    Fixed QR controller to mkdir, create new file, throw exceptions and return correct content type

--- Service/app/controllers/QRController.java
+++ Service/app/controllers/QRController.java
@@ -27,63 +27,62 @@ import play.mvc.Http.Header;
  *
  */
 public class QRController extends Controller {
-	
+
 	/** 
 	 * This function generates the QRcode of an URL based on the poll with a determined polltoken.
 	 * @param polltoken - The poll token to use for linking the QRCode image to the poll.
 	 * @param size - The size of the image.
 	 * @return - Returns the image or null if error.
 	 */
-	
-	private static BufferedImage generateImage (String polltoken, int size) {
+
+	private static BufferedImage generateImage (String polltoken, int size) throws Exception {
 		String httpheader = Http.Request.current().headers.get("host").value();
 		String URL = ("http://" + httpheader + "/" + polltoken);
-		
+
 		BitMatrix bm;
-        Writer writer = new QRCodeWriter();
+		Writer writer = new QRCodeWriter();
 		try {
 			bm = writer.encode(URL, BarcodeFormat.QR_CODE, size, size);
-		
-        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        
-        for (int y = 0; y < size; y++) {
-            for (int x = 0; x < size; x++) {
-                int grayValue = (bm.get(x, y) ? 1 : 0) & 0xff;
-                image.setRGB(x, y, (grayValue == 0 ?  0xFFFFFF : 0));
-            }
-        }
 
-		return image;
-		
+			BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+
+			for (int y = 0; y < size; y++) {
+				for (int x = 0; x < size; x++) {
+					int grayValue = (bm.get(x, y) ? 1 : 0) & 0xff;
+					image.setRGB(x, y, (grayValue == 0 ?  0xFFFFFF : 0));
+				}
+			}
+
+			return image;
+
 		} catch (Exception e) {
-            e.printStackTrace(System.err);
+			e.printStackTrace(System.err);
 		}
 		return null;
 	}
-	
+
 	/**
 	 * This function is called from the Routes file, it creates a file in the tmp folder with the image of
 	 * the QRCode if it doesn't exist, if it does, uses it. 
 	 */
-	
-	public static void returnImage () {
-		try {
-			String polltoken = params.get("token");
-			int size = params.get("size", Integer.class).intValue();
 
-			String path = Play.applicationPath.getPath();
-			boolean exists = (new File(path + "/tmp/qrcodes/" + polltoken + "_" + size + ".jpg")).exists();
+	public static void returnImage() throws Exception {
+		String polltoken = params.get("token");
+		int size = params.get("size", Integer.class).intValue();
 
-			if (!exists) {
-				FileOutputStream fos = new FileOutputStream(path +  "/tmp/qrcodes/" + polltoken + "_" + size + ".jpg");
-				BufferedImage image = generateImage(polltoken, size);
-				ImageIO.write(image, "jpg", fos);
-				fos.close();
-			}
-			File f = new File(path + "/tmp/qrcodes/" + polltoken + "_" + size + ".jpg");
-			renderBinary(f);
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		} 
+		String path = Play.applicationPath.getPath();
+		File dir = new File(path+"/tmp/qrcodes/");
+		if (!dir.isDirectory())
+			dir.mkdir();
+		
+		File f = new File(path + "/tmp/qrcodes/" + polltoken + "_" + size + ".jpg");
+		
+		if (!f.exists()) {
+			FileOutputStream fos = new FileOutputStream(f);
+			BufferedImage image = generateImage(polltoken, size);
+			ImageIO.write(image, "jpg", fos);
+			fos.close();
+		}
+		renderBinary(f);
 	}
 }
