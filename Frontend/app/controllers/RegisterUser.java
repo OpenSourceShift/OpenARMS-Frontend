ad4247fdb5977a2edab37dee0e0bd84eb9d60e03
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2012-03-21 11:59:40 +0200

    change class name

--- Frontend/app/controllers/RegisterUser.java
+++ Frontend/app/controllers/RegisterUser.java
@@ -3,8 +3,7 @@ import play.mvc.Controller;
 
 import com.google.gson.JsonParseException;
 
-@Deprecated
-public class Poll extends Controller {
+public class RegisterUser extends Controller {
 	public static void getRegisterInf(String email, String passw, String confpassw) throws JsonParseException {
 
 	}
