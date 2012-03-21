f44140ec64eb44eb722239d588385fc98af1edef
Attila Sukosd <attila.sukosd@gmail.com>
2012-03-21 18:21:00 +0100

    Frontend testing code

--- Frontend/test/APIClientTest.java
+++ Frontend/test/APIClientTest.java
@@ -1,5 +1,7 @@
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -23,6 +25,71 @@ public class APIClientTest extends UnitTest {
     		fail("did not get the HTTP-OK status-code from the service, got "+response.statusCode+": "+response.error_message);
 		}
 	}
+	
+	@Test
+	public void testCreateFullConfig() throws Exception {
+		// Create user
+		UserJSON u = new UserJSON();
+		u.name = "openarms";
+		u.email = "test@test.com";
+		u.secret = null;
+		u.attributes = new HashMap<String, String>();
+		u.attributes.put("password", "1234");
+		u.backend = "class models.SimpleUserAuthBinding";
+
+		CreateUserResponse userresponse = (CreateUserResponse) APIClient.send(new CreateUserRequest(u));
+
+    	failIfNotSuccessful(userresponse);
+    	assertNotNull(userresponse.user.id);
+    	assertEquals(userresponse.user.name, u.name);
+    	// Authenticate
+		APIClient apiClient = new APIClient();
+		boolean authenticated = apiClient.authenticateSimple(u.email, u.attributes.get("password"));
+		assertTrue(authenticated);
+    
+    	PollJSON pj1 = new PollJSON();
+    	pj1.question = "This is the first question.";
+    	pj1.admin = userresponse.user.id;
+    	CreatePollResponse pollresponse = (CreatePollResponse) apiClient.sendRequest(new CreatePollRequest(pj1));
+    	failIfNotSuccessful(pollresponse);
+    	assertEquals(pj1.question, pollresponse.poll.question);
+    	assertNotNull(pollresponse.poll.id);
+    	
+    	CreateChoiceResponse choiceresponse = null;
+    	for(int i = 0; i < 4; i++) {
+        	ChoiceJSON c = new ChoiceJSON();
+        	c.text = "choice text "+i;
+        	c.poll_id = pollresponse.poll.id;
+        	c.correct = (i == 0 ? true : false);
+        	choiceresponse = (CreateChoiceResponse) APIClient.send(new CreateChoiceRequest(c));
+        	failIfNotSuccessful(choiceresponse);
+        	assertNotNull(choiceresponse.choice.id);
+        	assertEquals(choiceresponse.choice.text, c.text);
+    	}
+    	
+    	PollInstanceJSON p = new PollInstanceJSON();
+    	p.poll_id = pollresponse.poll.id;
+    	p.start = new Date(0);
+    	p.end = new Date();
+    	CreatePollInstanceResponse piresponse = (CreatePollInstanceResponse) APIClient.send(new CreatePollInstanceRequest(p));
+    	failIfNotSuccessful(piresponse);
+    	assertNotNull(piresponse.pollinstance.id);
+    	assertEquals(piresponse.pollinstance.poll_id, p.poll_id);
+    	
+    	Random r = new Random();
+    	for(int i=0;i<20;i++) {
+        	VoteJSON v = new VoteJSON();
+        	v.choiceid =  1+(long)(Math.random()*3);
+        	v.pollInstanceid = piresponse.pollinstance.id;
+        	CreateVoteResponse vresponse = (CreateVoteResponse) APIClient.send(new CreateVoteRequest(v));
+        	failIfNotSuccessful(vresponse);
+        	assertNotNull(vresponse.vote.id);
+        	assertEquals(vresponse.vote.choiceid, v.choiceid);
+    	}
+    	
+    	
+	}
+	
 	/*
 	@Test
     public void testLogin() throws Exception {
@@ -65,12 +132,7 @@ public class APIClientTest extends UnitTest {
     	assertNotNull(response.pollinstance.id);
     	assertEquals(response.pollinstance.poll_id, p.poll_id);
     }
-<<<<<<< HEAD
-  	
-=======
-  	*/
-	/*
->>>>>>> 0b5d00f674abca40e14a4e385db8a6088ae373c4
+
 	@Test
     public void testCreateUser() throws Exception {
     	// WORKING
@@ -88,12 +150,7 @@ public class APIClientTest extends UnitTest {
     	assertNotNull(response.user.id);
     	assertEquals(response.user.name, u.name);
     }
-<<<<<<< HEAD
-	 
-=======
-    */
-	/*
->>>>>>> 0b5d00f674abca40e14a4e385db8a6088ae373c4
+
     @Test
     public void testCreatePoll() throws Exception {
     	//APIClient apiClient = new APIClient();
@@ -135,7 +192,7 @@ public class APIClientTest extends UnitTest {
     	assertNotNull(response.poll.token);
     	assertEquals(response.poll.token,token);
     }
-   */  
+   */ /* 
     @Test
     public void testEditPoll() throws Exception {
     	PollJSON p = new PollJSON();
@@ -153,7 +210,7 @@ public class APIClientTest extends UnitTest {
     	failIfNotSuccessful(response);
     	assertNotNull(response.poll.token);
     	assertEquals(response.poll.id,p.id);
-    }
+    } */
    /*
     @Test
     public void testCreateVote() throws Exception {
