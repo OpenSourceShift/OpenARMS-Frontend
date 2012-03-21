61dd000745379ac6981452a0c382a0416366815a
Ignacio Rodriguez <kronics777@gmail.com>
2012-03-21 16:28:52 +0100

    Changed ClonePollController.

--- Service/app/api/requests/ClonePollRequest.java
+++ Service/app/api/requests/ClonePollRequest.java
@@ -9,14 +9,14 @@ import api.responses.Response;
  * A request for the service: Do a clone of a poll, i.e. clone all properties of the poll, except the votes.
  */
 public class ClonePollRequest extends Request {
-	public PollJSON poll;
-	public ClonePollRequest (PollJSON p) {
-		this.poll = p;
+	public Long poll_id;
+	public ClonePollRequest (Long p) {
+		this.poll_id = p;
 	}
 		
 	@Override
 	public String getURL() {
-		return "/poll/" + poll.id + "/clone";
+		return "/poll/" + poll_id + "/clone";
 	}
 
 	@Override
