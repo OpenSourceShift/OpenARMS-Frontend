0e20ddba3f92534d436db2c6c9b1780d3959a77c
Kræn Hansen <post@creen.dk>
2012-03-22 20:29:17 +0100

    Create poll works

--- Frontend/app/controllers/CreatePoll.java
+++ Frontend/app/controllers/CreatePoll.java
@@ -11,31 +11,55 @@ import api.responses.CreatePollResponse;
 
 public class CreatePoll extends BaseController {
 
-	public static void index(String question, String[] answer) {
+	public static void index(String question, String[] choices) {
 		if (!LoginUser.isLoggedIn()) {
 			session.put("page_prior_to_login", request.url);
 			LoginUser.index(null);
 		}
-		String choices;
-		if (answer == null) {
-			answer = new String[] { "", "" };
+		PollJSON poll = new PollJSON();
+		if(question == null) {
+			poll.question = "";
+		} else {
+			poll.question = question;
 		}
+		poll.choices = new LinkedList<ChoiceJSON>();
+		if (choices != null) {
+			for(String c: choices) {
+				ChoiceJSON choiceJson = new ChoiceJSON();
+				choiceJson.text = c;
+				poll.choices.add(choiceJson);
+			}
+		} else {
+			// Default
+			ChoiceJSON c1 = new ChoiceJSON();
+			c1.text = "";
+			poll.choices.add(c1);
+			ChoiceJSON c2 = new ChoiceJSON();
+			c2.text = "";
+			poll.choices.add(c2);
+		}
+		// Default values.
+		poll.loginRequired = false;
+		poll.multipleAllowed = true;
+		/*
 		List<ChoiceJSON> choices_json = new LinkedList<ChoiceJSON>();
-		for(String c: answer) {
+		for(String c: choices) {
 			ChoiceJSON cJSON = new ChoiceJSON();
 			cJSON.text = c;
 			choices_json.add(cJSON);
 		}
-		choices = GsonHelper.toJson(choices_json);
-		render(question, answer, choices);
-
+		String pollJson = GsonHelper.toJson(poll);
+		render(poll, pollJson);
+		*/
+		String choicesJson = GsonHelper.toJson(poll.choices);
+		render(poll, choicesJson);
 	}
 
 	public static void success(String token) {
 		render(token);
 	}
 
-	public static void submit(String question, String[] answer, String type, Boolean loginRequired) {
+	public static void submit(String question, String[] choices, String type, Boolean loginRequired) {
 		if (!LoginUser.isLoggedIn()) {
 			session.put("page_prior_to_login", request.url);
 			LoginUser.index(null);
@@ -43,8 +67,8 @@ public class CreatePoll extends BaseController {
 				
 		// Remove empty lines from the answers
 		List<ChoiceJSON> choicesJson = new LinkedList<ChoiceJSON>();
-		if (answer != null) {
-			for (String s: answer) {
+		if (choices != null) {
+			for (String s: choices) {
 				if (s != null && !s.isEmpty()) {
 					ChoiceJSON choice = new ChoiceJSON();
 					choice.text = s;
@@ -59,14 +83,14 @@ public class CreatePoll extends BaseController {
 
 		// Validate that we have at least 2 answer options.
 		if (choicesJson.size() < 2) {
-			validation.addError("choices", "validation.required.atLeastTwo", answer);
+			validation.addError("choices", "validation.required.atLeastTwo", choices);
 		}
 
 		// If we have an error, go to newpollform.
 		if (validation.hasErrors()) {
 			params.flash();
 			validation.keep();
-			index(question, answer);
+			index(question, choices);
 			return;
 		}
 		
@@ -102,7 +126,7 @@ public class CreatePoll extends BaseController {
 			params.flash();
 			validation.addError(null, e.getMessage());
 			validation.keep();
-			index(question, answer);
+			index(question, choices);
 		}
 	}
 }
