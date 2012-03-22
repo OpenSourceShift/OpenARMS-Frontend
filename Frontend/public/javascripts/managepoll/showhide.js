3d04471f4439c0b72fd2ff8915a4882d49a9a076
Kræn Hansen <post@creen.dk>
2012-03-22 21:21:21 +0100

    Overhauled the manage poll css

--- Frontend/public/javascripts/managepoll/showhide.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('div.instances').hide();  
-  $('.questionwith').click(function() {
-	  // jQuery noob at work
-    $(this).next().next().next("div.instances").slideToggle('slow');
-  });
-  
-  $("button").button();
-});
