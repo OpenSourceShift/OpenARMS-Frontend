0914875d0eba8f6f4f2cd21e9489088d2b29169e
Ignacio Rodriguez <kronics777@gmail.com>
2012-03-22 10:52:41 +0100

    A user can't vote twice in the same poll implemented.

--- Service/app/api/responses/VoteOnPollInstanceResponse.java
+++ Service/app/api/responses/VoteOnPollInstanceResponse.java
@@ -1,6 +1,15 @@
 package api.responses;
 
+import api.entities.PollJSON;
+
 
 
 public class VoteOnPollInstanceResponse extends CreateVoteResponse {
+	public PollJSON poll;
+	public VoteOnPollInstanceResponse() {
+	}
+
+	public VoteOnPollInstanceResponse(PollJSON json) {
+		this.poll = json;
+	}
 }
