b8d34cd8008d13d5b9f24aa867e916416cd8786b
Christian Holm Christiansen <christianholmchristiansen@gmail.com>
2012-08-15 17:18:29 +0200

    Changed to use Play validation instead of static methods

--- Frontend/app/controllers/RegisterUser.java
+++ Frontend/app/controllers/RegisterUser.java
@@ -15,25 +15,13 @@ import com.google.gson.JsonParseException;
 
 public class RegisterUser extends BaseController {
 	public static void submit(String name, String email, String passw, String confpassw) throws Exception {
-		boolean changed = false;
-		if (!checkname(name)) {
-			validation.addError("nameError", "Invalid user name");
-		}
 		
-		if(!checkpassw(passw, confpassw)){
-			validation.addError("passwDontMatch", "Passwords doesn't match");
-			changed = true;
-		}
-		if(!checkemail(email)){
-			validation.addError("email", "Wrong email");
-			changed = true;
-		}
-		if(!validatePass(passw)){
-			validation.addError("validatePass", "Password is short");
-			changed = true;
-		}
+		validation.minSize(name, 4);	// TODO: Change to use regular expressions so as to exclude profanity
+		validation.equals(passw, confpassw);
+		validation.email(email);
+		validation.minSize(email, 4); 	// TODO: Change to use regular expressions
 
-		if(!changed){
+		if(!validation.hasErrors()){
 			UserJSON uj = new UserJSON();
 			uj.name = name;
 			uj.email = email;
@@ -52,34 +40,11 @@ public class RegisterUser extends BaseController {
 			showform();
 		}
 	}
-	// TODO: proper name check, maybe against service?
-	private static boolean checkname(String name) {
-		if (name.length() < 3)
-			return false;
-		return true;
-	}
+
 	public static void showform(){
 		render();
 	}
 	public static void success(){
 		render();
-	}
-
-	//does passwords match?
-	// TODO: proper password checks
-	public static boolean checkpassw(String passw, String confpassw){
-		return passw.equals(confpassw);
-	}
-	//check wrong symbols and length
-	public static boolean validatePass(String passw){
-		int length = passw.length();
-		return (length > 4);
-	}
-	// TODO: proper email validation check, against the service also?
-	public static boolean checkemail(String email) {
-		Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
-		Matcher m = p.matcher(email);
-		boolean result = m.matches();
-		return result;
 	}
 }
