a2cba0da5bbd8903415193fc2835e73cd151a555
Kræn Hansen <post@creen.dk>
2012-08-26 22:52:31 +0200

    Added the stress.openarms.dk as a allowed origin.

--- Frontend/app/controllers/BaseController.java
+++ Frontend/app/controllers/BaseController.java
@@ -5,6 +5,7 @@ import play.Logger;
 import play.mvc.Before;
 import play.mvc.Catch;
 import play.mvc.Controller;
+import play.mvc.Finally;
 
 public abstract class BaseController extends Controller {
 	@Before
@@ -16,5 +17,11 @@ public abstract class BaseController extends Controller {
 	public static void renderAPIException(Exception e) {
 		renderText("Catched an exception: "+e.getMessage());
 		//Logger.debug("Catched an exception: ", e);
+	}
+	
+	@Finally
+	public static void addSOPHeader() {
+		//response.headers.put("Access-Control-Allow-Origin", new Header(name, value))
+		response.setHeader("Access-Control-Allow-Origin", "http://stress.openarms.dk/");
 	}
 }
